8aaab1a74e9e8cd15932f86483182327c12db092
Tri <vantri@tuta.io>
2017-07-25 19:51:35 +0700

    Fix youtube get link

--- public/javascripts/testarea.js
+++
@@ -1,6 +0,0 @@
-function urlParam (url, name){
-	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(url);
-	return results[1] || 0;
-}
-
-console.log(urlParam('https://www.youtube.com/watch?v=uD6Qzt1vjGM&index=91&list=LLBk4NE5mZeAI7z8zXqmdUOg','v'))
